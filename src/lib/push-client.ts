78a8f7bf2f3128ddfeb436cc0b725f93e11d40f6
Suryansh2002 <suryanshwins2002@gmail.com>
2025-07-30 23:10:12 +0530

    .

--- src/lib/push-client.ts
+++ src/lib/push-client.ts
@@ -6,11 +6,11 @@ function isNotificationSupported() {
 }
 
 async function enablePushNotifications() {
-    const registration = await navigator.serviceWorker.register("/service-worker.js");
     const permission = await Notification.requestPermission();
     if (permission !== "granted"){
         return;
     }
+    const registration = await navigator.serviceWorker.register("/service-worker.js");
     const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: process.env.NEXT_PUBLIC_VAPID_KEY
